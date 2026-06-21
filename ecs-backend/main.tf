f39e05c71e01e7de76ffff3c28698562915052f0
Adeleye <leymandudu@gmail.com>
2026-06-21 14:33:02 -0400

    Update ecs-backend/.terraform.lock.hcl

--- ecs-backend/main.tf
+++ ecs-backend/main.tf
@@ -314,19 +314,15 @@ resource "aws_lb_target_group" "backend" {
   }
 }
 
-# Redirect all HTTP traffic to HTTPS
+# Forward HTTP traffic to backend (used by CloudFront origin)
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    type = "redirect"
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.backend.arn
   }
 }
 
